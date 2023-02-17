e8d4222b00670aa65993818770d17dfb8c23dfec
SkyKeyUA <veremiienkoov96@gmail.com>
2023-02-17 12:09:36 +0200

    Сounter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+/** @format */
+
+import React from 'react';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const onClickMinus = () => {
+    setCount((prev) => prev - 1);
+  };
+  const onClickPlus = () => {
+    setCount((prev) => prev + 1);
+  };
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Сounter:</h2>
+        <h1>{count}</h1>
+        <button disabled={!count} onClick={onClickMinus} className="minus">
+          - Minus
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          Plus +
+        </button>
       </div>
     </div>
   );
