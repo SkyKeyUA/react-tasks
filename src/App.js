8b2a8827b2dcf349f31e09683cb1d1a18aa75bd7
SkyKeyUA <veremiienkoov96@gmail.com>
2023-02-21 12:03:36 +0200

    users

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
@@ -6,10 +8,53 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetch('https://reqres.in/api/users')
+          .then((res) => res.json())
+          .then((json) => setUsers(json.data));
+        setIsLoading(false);
+      } catch (error) {
+        alert('error when requesting data');
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+  const onClickSendInvites = () => {
+    setSuccess(!success);
+  };
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success onClickSendInvites={onClickSendInvites} count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchInput={onChangeSearchInput}
+          searchValue={searchValue}
+          isLoading={isLoading}
+          items={users}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,12 +1,16 @@
+/** @format */
+
 import React from 'react';
 
-export const Success = ({ count }) => {
+export const Success = ({ count, onClickSendInvites }) => {
   return (
-    <div class="success-block">
+    <div className="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <button onClick={onClickSendInvites} className="send-invite-btn">
+        Назад
+      </button>
     </div>
   );
 };
